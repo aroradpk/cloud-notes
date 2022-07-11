bca7b53e8c2f73ae8360b81e5d8543b13b98102d
Deepak <aroradeepak485@gmail.com>
2022-07-12 03:01:48 +0530

    Deleting a note function

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -89,8 +89,10 @@ const NoteState = (props) => {
   }
 
   //Delete a note
-  const deleteNote = () => {
-
+  const deleteNote = (id) => {
+    console.log("Deleting the note with " + id);
+    const newNotes = notes.filter((note)=> { return note._id !== id});
+    setNotes(newNotes);
   }
   //Edit a note
   const editNote = () => {
